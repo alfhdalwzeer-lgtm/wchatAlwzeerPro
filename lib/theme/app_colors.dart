e0aa8564c2cdcf8484d6a48076c8b96ee99415f7
alfhdalwzeer-lgtm <alfhdalwzeer@gmail.com>
2026-09-18 17:28:56 +0300

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const gold = Color(0xFFFFC107);
+
+  static const goldDark = Color(0xFF8D7000);
+
+  static const header = Color(0xFF1F2C34);
+
+  static const background = Color(0xFF0F1417);
+
+  static const card = Color(0xFF202E36);
+
+  static const incoming = Color(0xFF202E36);
+
+  static const outgoing = Color(0xFF006B5B);
+
+  static const muted = Color(0xFFB9B5C2);
+}
