a3ef661820eca9bd6bb1e346484f5bb2acd9cfe8
alfhdalwzeer-lgtm <alfhdalwzeer@gmail.com>
2026-09-18 19:11:48 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+   return MaterialApp(
       title: 'wchatAlwzeerPro',
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
