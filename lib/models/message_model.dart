ea73deb841b48811ea5ed0e1e8534a076c10dc55
alfhdalwzeer-lgtm <alfhdalwzeer@gmail.com>
2026-09-20 21:15:54 +0300

    Update message_model.dart

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,13 +1,28 @@
+cd ~/wchatAlwzeerPro
+cat > lib/models/message_model.dart <<'EOF'
 class MessageModel {
   final String text;
   final bool isMe;
   final String time;
   final bool isRead;
 
+  // نوع الرسالة: text / image / file
+  final String type;
+
+  // مسار الصورة أو الملف على الجهاز
+  final String? filePath;
+
+  // اسم الملف عند إرسال مستند
+  final String? fileName;
+
   MessageModel({
     required this.text,
     required this.isMe,
     required this.time,
     this.isRead = false,
+    this.type = 'text',
+    this.filePath,
+    this.fileName,
   });
 }
+EOF
