a159dd7b9174fd3c36db9f9c21140f6f15d28f5c
alfhdalwzeer-lgtm <alfhdalwzeer@gmail.com>
2026-09-21 17:24:12 +0300

    Refactor MessageModel class constructor and fields

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,21 +1,18 @@
-cd ~/wchatAlwzeerPro
-cat > lib/models/message_model.dart <<'EOF'
+cd ~/wchatAlwzeerPro && cat > lib/models/message_model.dart <<'EOF'
 class MessageModel {
+  final int? id;
+  final String chatId;
   final String text;
   final bool isMe;
   final String time;
   final bool isRead;
-
-  // نوع الرسالة: text / image / file
   final String type;
-
-  // مسار الصورة أو الملف على الجهاز
   final String? filePath;
-
-  // اسم الملف عند إرسال مستند
   final String? fileName;
 
   MessageModel({
+    this.id,
+    required this.chatId,
     required this.text,
     required this.isMe,
     required this.time,
